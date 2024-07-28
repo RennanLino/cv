778c2dadf3b03cc0caa2f3190fc5f5b56f3edf11
Rennan Lino <rlino@hie.tec.br>
2024-07-27 22:59:12 -0300

    remove: quebra de pagina na seção de cursos e site pessoal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -166,7 +166,6 @@ export default function Page() {
             })}
           </div>
         </Section>
-
         <Section className="print-force-new-page scroll-mb-16">
           <h2 className="text-xl font-bold">Cursos de Extensão</h2>
           <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
@@ -187,10 +186,10 @@ export default function Page() {
 
       <CommandMenu
         links={[
-          {
-            url: RESUME_DATA.personalWebsiteUrl,
-            title: "Personal Website",
-          },
+          // {
+          //   url: RESUME_DATA.personalWebsiteUrl,
+          //   title: "Personal Website",
+          // },
           ...RESUME_DATA.contact.social.map((socialMediaLink) => ({
             url: socialMediaLink.url,
             title: socialMediaLink.name,
